eb0ddb051993d891736aff92d8dc07ffc75cd8c3
Ghozi <mghozighifar04@gmail.com>
2023-12-27 18:09:24 +0700

    Fungsi komposabel untuk formulir input siswa.

--- app/src/main/java/com/example/restapi/ui/home/screen/InsertScreen.kt
+++ app/src/main/java/com/example/restapi/ui/home/screen/InsertScreen.kt
@@ -1,2 +1,15 @@
 package com.example.restapi.ui.home.screen
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.restapi.ui.home.viewmodel.InsertUiEvent
+
+@Composable
+fun FormInputSiswa(
+    insertUiEvent: InsertUiEvent,
+    modifier: Modifier = Modifier,
+    onValueChange : (InsertUiEvent) -> Unit = {},
+    enable : Boolean = true
+){
+
+}
